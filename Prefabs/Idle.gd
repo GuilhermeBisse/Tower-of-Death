29e54e3e858d897c8349908d1100b192de49dc30
Gustavo Salmazo <gux.salmazo@gmail.com>
2024-10-29 15:13:39 -0300

    Transition Idle - SincPunch

--- Prefabs/Idle.gd
+++ Prefabs/Idle.gd
@@ -1,6 +1,20 @@
 extends State
+@onready var animation_player = $"../../AnimationPlayer"
 
+var is_attacking = false
 
+func enter():
+	super.enter()
+	animation_player.play("Idle")
 
 func _on_attack_timer_timeout():
-	pass
+	is_attacking = true
+	
+func transition():
+	if is_attacking:
+		get_parent().change_state("SincPunch")
+		is_attacking = false
+		
+func exit():
+	super.exit()
+	owner.set_physics_process(false)
---
+++ Prefabs/SincPunch.gd
@@ -0,0 +1,21 @@
+extends State
+@onready var animation_player = $"../../AnimationPlayer"
+var attack_finished = false
+func enter():
+	super.enter()
+	owner.set_physics_process(true)
+	animation_player.play("SincPunch")
+	
+func exit():
+	super.exit()
+	owner.set_physics_process(false)
+	
+func transition():
+	if attack_finished:
+		get_parent().change_state("Idle")
+		print("Now its idling")
+		attack_finished = false
+
+
+func _on_animation_player_animation_finished(anim_name):
+	attack_finished = true
