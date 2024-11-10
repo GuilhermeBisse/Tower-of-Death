157cffde048889d0f6259856900508b3a35f9352
Gustavo Salmazo <gux.salmazo@gmail.com>
2024-11-09 22:04:26 -0300

    Right/Left push + Press attack

---
+++ Prefabs/PressAttack.gd
@@ -0,0 +1,17 @@
+extends State
+
+var attack_finished = false
+@onready var animation_player = $"../../AnimationPlayer"
+
+func enter():
+	super.enter()
+	attack_finished = false
+	owner.set_physics_process(false)
+	animation_player.play("PressAttack")
+
+func transition():
+	if attack_finished:
+		get_parent().change_state("Idle")
+
+func _on_animation_player_animation_finished(anim_name):
+	attack_finished = true
--- Scripts/Idle.gd
+++ Scripts/Idle.gd
@@ -3,7 +3,7 @@ extends State
 @onready var attack_timer = $"../../AttackTimer"
 var rng
 var is_attacking = false
-var attacks = ["SincPunch", "RightPunch", "LeftPunch"];
+var attacks = ["SincPunch", "RightPunch", "LeftPunch", "LeftPush", "RightPush", "PressAttack"];
 func enter():
 	randomize()
 	super.enter()
@@ -14,9 +14,8 @@ func enter():
 
 func _on_attack_timer_timeout():
 	is_attacking = true
-	attack_timer.wait_time = randf_range(5,10)
+	attack_timer.wait_time = randf_range(3,5)
 	print("it will attack! and its new time for attack is ", attack_timer.wait_time)
-	owner.set_physics_process(false)
 	
 func transition():
 	if is_attacking:
